f1893e134a1d31de53b4e1fb54464d51ef147d50
KaushikRishabh <reachmeatrish@gmail.com>
2022-11-15 20:26:29 +0530

    testing coingecko api

--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+// import logo from "./logo.svg";
+import "./App.css";
+import { useState, useEffect } from "react";
+import axios from "axios";
 
 function App() {
+  //let coinData = [];
+  const [coinData, setCoinData] = useState([]);
+  const coinListURL = "https://api.coingecko.com/api/v3/coins/list";
+  useEffect(() => {
+    axios.get(coinListURL).then((response) => {
+      // setCoinData(response.data);
+      const eth = coinFilter(response.data);
+      setCoinData(eth);
+      console.log(response.data);
+    });
+  }, []);
+
+  const coinFilter = (data) => {
+    return data.filter(
+      (coin) =>
+        coin.symbol === "eth" || coin.symbol === "btc" || coin.symbol === "ada"
+    );
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {coinData.length && (
+        <ol>
+          {coinData.map((coin) => (
+            <li key={coin.id}>
+              <span>{coin.id}</span>
+              <span>{coin.name}</span>
+              <span>{coin.symbol}</span>
+            </li>
+          ))}
+        </ol>
+      )}
     </div>
   );
 }
